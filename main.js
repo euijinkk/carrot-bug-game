b58e747239c1c0fc38c94c39ad0fc151e75aeeaa
gytks4 <gytks4@naver.com>
2021-04-05 13:28:30 +0900

    background border radius, sound, scale(1.1)

--- main.js
+++ main.js
@@ -41,10 +41,17 @@ gameover.appendChild(redoBtn);
 gameover.appendChild(paragraph);
 redoBtn.appendChild(redoIcon);
 
+let alertFile = new Audio('sound/alert.wav');
+let bgFile = new Audio('sound/bg.mp3');
+let bugFile = new Audio('sound/bug_pull.mp3');
+let carrotFile = new Audio('sound/carrot_pull.mp3');
+let gameWinFile = new Audio('sound/game_win.mp3');
+
+
 
 
 playBtn.addEventListener('click', () => {
-    play()
+    playGame()
 })
 
 redoBtn.addEventListener('click', () =>{
@@ -55,7 +62,8 @@ redoBtn.addEventListener('click', () =>{
     
     playBtn.classList.add("first");
     timer.innerHTML = "10:00"
-    play();
+    bgFile.currentTime =0;
+    playGame();
     
 })
 
@@ -93,21 +101,32 @@ function addZero(num) {
     return (num < 10 ? '0'+num : ''+num)
     }
     
-function play() {
+function playGame() {
+    alertFile.play();
     if(playBtn.classList.contains("first")){
+        // 첫시작
         makeThing("carrot",carrotNum);
         makeThing("bug",bugNum);
         
         playBtn.childNodes[1].className = playBtn.childNodes[1].className.replace("play","stop")
         playBtn.classList.remove("first");
         carrotContainer.addEventListener('click', (e) => {onCarrotClick(e.target);})
-        bugContainer.addEventListener('click', () => message("YOU LOSE"));
+        bugContainer.addEventListener('click', () => {
+            bugFile.play();
+            message("YOU LOSE")
+        }
+            );
         timerFunc.startTimer();
+        bgFile.play();
     } else if (playBtn.childNodes[1].classList.contains("fa-play")) {
+        // 재시작
+        bgFile.play();
         playBtn.childNodes[1].className = playBtn.childNodes[1].className.replace("play","stop")
         carrotContainer.addEventListener('click', (e) => {onCarrotClick(e.target);})
         timerFunc.startTimer();
     } else if (playBtn.childNodes[1].classList.contains("fa-stop")){
+        // 중지
+        bgFile.pause();
         playBtn.childNodes[1].className = playBtn.childNodes[1].className.replace("stop","play")
         timerFunc.stopTimer();
     }
@@ -118,6 +137,7 @@ function onCarrotClick(target){
     const toBeDeleted = document.querySelector(`.carrot[data-id="${target.dataset.id}"]`);
     toBeDeleted.remove();
     leftCarrotNum.innerHTML = carrotContainer.childNodes.length;
+    carrotFile.play();
     if(leftCarrotNum.innerHTML === "0") {
         message("YOU WIN")
     }
@@ -127,7 +147,10 @@ function message(message) {
     paragraph.innerHTML = message;
     background.appendChild(gameover);
     timerFunc.stopTimer();
-
+    bgFile.pause();
+    if (message==="YOU WIN") {
+        gameWinFile.play();
+    }
     // why is it not working?
     carrotContainer.removeEventListener('click', (e) => {onCarrotClick(e.target);});
     bugContainer.removeEventListener('click', () => message("YOU LOSE"));
@@ -162,3 +185,6 @@ function getRandomArbitrary(min, max) {
 // 1. 이미지가 정사각형으로 인식되어, 이기는게 불가능할 수 있다.
 // 2. 화면이 resize되면, background image 만 움직이고 당근/벌레는 따라오지 않는 문제.
 // 3. 멈추었을 때, 당근/벌레가 여전히 클릭되는 문제 - removeEventListener is now working.
+// 4. 재생버튼, hover -> 1.1배
+// 5. sound 
+// 6. 스페이스바를 누르면 게임이 시작되도록.
